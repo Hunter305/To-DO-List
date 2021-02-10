cbf4d60c73207fd1ccde064257ceb79b0f12989e
Hunter305 <avinash8169@gmail.com>
2021-02-10 13:02:15 +0530

    Final Commit

--- app.js
+++ app.js
@@ -2,43 +2,53 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require("body-parser")
-let items=["EAT","SLEEP"];
-let workItems=[];
+const date = require(__dirname + "/views/date.js")
+
+const items = ["EAT", "SLEEP"];
+const workItems = [];
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+
 app.get('/', function(req, res) {
-  let today = new Date();
-  let options={weekday:"long",day:"numeric",month:"long"};
-  let Day=today.toLocaleDateString("en-US",options);
-  res.render("index", {listTitle: Day,newListItem:items});
+  let day = date.getDate()
+  res.render("index", {
+    listTitle: day,
+    newListItem: items
+  });
 });
 
-app.post("/", function(req,res){
+app.post("/", function(req, res) {
   console.log(req.body)
-  let item=req.body.newItem
-if(req.body.list==="Work"){
-  workItems.push(item);
-  res.redirect("/work")
-}else{
-  items.push(item)
-  res.redirect("/")
-}
+  let item = req.body.newItem
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    res.redirect("/")
+  }
 
 });
 
-app.get("/work",function(req,res){
-  res.render("index",{listTitle:"Work List",newListItem:workItems});
+app.get("/work", function(req, res) {
+  res.render("index", {
+    listTitle: "Work List",
+    newListItem: workItems
+  });
 });
 
-app.get("/about",function(req,res){
+app.get("/about", function(req, res) {
   res.render("about")
 })
 
-app.post("/work",function(req,res){
-let item=req.body.newItem;
-workItems.pust(item);
-res.redirect("/work")
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.pust(item);
+  res.redirect("/work")
 })
 
 app.listen(port, () => {
